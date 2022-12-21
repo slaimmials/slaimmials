20d1e5456de146b2722b23c2c64ac3bc4b851c58
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-21 16:48:45 +0300

    Update cat.lua

--- cat.lua
+++ cat.lua
@@ -1,4 +1,7 @@
-local as = game.Players.LocalPlayer.Backpack.Assault.Handle
-as.MeshId = "rbxassetid://11698310461"
-as.TextureID = "rbxassetid://11698310553"
-as.Size = Vector3.new(0.001, 0.001, 0.004)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+	if v.ClassName == "MeshPart" then
+		v.MeshId = "rbxassetid://11698310461"
+		v.TextureID = "rbxassetid://11698310553"
+		v.Size = Vector3.new(0.001, 0.001, 0.001)
+	end
+end
