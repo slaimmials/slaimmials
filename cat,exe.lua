57f8779268823da73f9c5a8d01b6179f6d3f1dc5
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-21 16:41:08 +0300

    Create cat,exe.lua

---
+++ cat,exe.lua
@@ -0,0 +1,4 @@
+local as = game.Players.LocalPlayer.Backpack.Assault.Handle
+as.MeshId = "rbxassetid://11698310461"
+as.TextureID = "rbxassetid://11698310553"
+as.Size = Vector3.new(0.001, 0.001, 0.004)
