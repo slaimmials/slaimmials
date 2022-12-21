0d46abd6bed8f7dfdfb9d9fd5288ffbc29b5459c
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-21 16:56:08 +0300

    Update cat.lua

--- cat.lua
+++ cat.lua
@@ -5,3 +5,10 @@ for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
 		v.Size = Vector3.new(0.001, 0.001, 0.001)
 	end
 end
+for i,v in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
+	if v.ClassName == "MeshPart" then
+		v.MeshId = "rbxassetid://11698310461"
+		v.TextureID = "rbxassetid://11698310553"
+		v.Size = Vector3.new(0.001, 0.001, 0.001)
+	end
+end
