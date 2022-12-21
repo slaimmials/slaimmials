479078837c13f051592b78bf743beef924c7d9d8
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-21 16:23:32 +0300

    Update imasd.lua

--- imasd.lua
+++ imasd.lua
@@ -1,2 +1,5 @@
-local new = nil
-new = Instance.new("Highlight", game.Players.LocalPlayer.Character)
+local head = game.Players.LocalPlayer.Character.Head
+head.MeshId = "rbxassetid://11698310461"
+head.TextureID = "rbxassetid://11698310553"
+head.Size = Vector3.new(0.003, 0.003, 0.003)
+
