ec809d64f331a83d5acb8bd3d8063ef855fd07df
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-24 11:24:03 +0300

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").Slaimmials2.PlayerGui.ScreenGui.UpdatesScroll.TextLabel.Text = "test"
+game:GetService("Players").Slaimmials2.PlayerGui.ScreenGui.UpdatesScroll.TextLabel.Text = "[+] Fixed ESP lag"
