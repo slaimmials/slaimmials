7cd2487e8478e6c4fad4d76890d42f1facf12399
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-29 15:33:41 +0300

    Create lAp.lua

---
+++ lAp.lua
@@ -0,0 +1,11 @@
+local password = nil
+local login = nil
+login = Instance.new("StringValue", game:GetService("TestService"))
+login.Name = "l1"
+login.Value = "D3LTAREX"
+login = nil
+
+password = Instance.new("StringValue", game:GetService("TestService"))
+password.Name = "p1"
+password.Value = "d3lTa13577531"
+password = nil
