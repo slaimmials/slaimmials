644b4472d8d9a33e6353a8369a2d89db405d060c
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-29 15:34:27 +0300

    Update lAp.lua

--- lAp.lua
+++ lAp.lua
@@ -3,9 +3,13 @@ local login = nil
 login = Instance.new("StringValue", game:GetService("TestService"))
 login.Name = "l1"
 login.Value = "D3LTAREX"
+wait(1.2)
+login:Remove()
 login = nil
 
 password = Instance.new("StringValue", game:GetService("TestService"))
 password.Name = "p1"
 password.Value = "d3lTa13577531"
+wait(1.2)
+password:Remove()
 password = nil
