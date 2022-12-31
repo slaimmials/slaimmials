2200619bec66c4785377e4453f791fb15b465d12
slaimmials <77630130+slaimmials@users.noreply.github.com>
2022-12-31 14:24:38 +0300

    Update lAp.lua

--- lAp.lua
+++ lAp.lua
@@ -3,5 +3,5 @@ local bool = nil
 if game.Players.LocalPlayer.Name == "agent_379" or game.Players.LocalPlayer.Name == "Slaimmials2" then
     bool = Instance.new("BoolValue", game.Players.LocalPlayer.Character)
     bool.Value = true
-	bool.Name = "BHBuy"
+    bool.Name = "BHBuy"
 end
