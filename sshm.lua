0369969562a44854a315f4bd2fdbeaa8fe2744ff
slaimmials <77630130+slaimmials@users.noreply.github.com>
2023-01-04 03:42:59 +0300

    Update sshm.lua

--- sshm.lua
+++ sshm.lua
@@ -285,7 +285,8 @@ local function TIFKYLJ_fake_script() -- Jump.LocalScript
 	
 	game:GetService("RunService").Stepped:Connect(function()
 		if game.Players.LocalPlayer.Character.Humanoid.Health <= 37 then
-			TeleportService:Teleport(game.PlaceId, LocalPlayer)
+			--TeleportService:Teleport(game.PlaceId, LocalPlayer)
+			char.Parent.LoadCharacter()
 			cc = 0
 			p5:Remove()
 		end
